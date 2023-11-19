949707d3f2ff77ce35ca9cef0069279decb5eee3
Md Mahbub Alom <mdmahbuba034@gmail.com>
2023-11-20 01:38:07 +0600

    loading skeletion make done

---
+++ src/app/metadata/page.js
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: 'meta data test',
+  description: 'meta data test NEXT.JS (13.4.4) Example',
+}
+
+export default function metaData() {
+  return (
+    <div>
+<h2>Meta data page</h2>
+    </div>
+  );
+}
---
+++ src/app/userlist/page.js
@@ -0,0 +1,29 @@
+
+export const metadata = {
+    title: 'Skeleton Loading Products',
+    description: 'Skeleton Loading NEXT.JS (13.4.4) Example',
+}
+
+
+async function userList() {
+  let data = await fetch("https://dummyjson.com/users");
+  data = await data.json();
+  return data.users;
+}
+
+export default async function Page() {
+  let users = await userList();
+  console.log(users);
+  return (
+    <div>
+      <h1>User List Here:</h1>
+      {
+        users.map((item)=>(
+            <div>
+                <h2>First Name: {item.firstName}</h2>
+            </div>
+        ))
+      }
+    </div>
+  );
+}
